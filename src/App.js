528cce36bb91749e298d03853a03cd29b305f92c
Finn Farrell <fifarrel@tcd.ie>
2024-02-12 19:00:47 +0000

    Version 1 of To Do App in React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Widget from "./components/widget/Widget";
+import AddButton from "./components/add-button/AddButton";
+import s from "./style.module.css";
 
 function App() {
+  const [list, SetList] = useState([]);
+  const addToList = (newItem) => {
+    SetList((list) => [newItem, ...list]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={s.main}>
+      <div className={s.content}>
+        <AddButton returnNewItem={addToList} />
+        <div>
+          {list.map((widget) => {
+            return <Widget title={widget.title} due={widget.due} />;
+          })}
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+// ;
